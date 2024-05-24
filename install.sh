2bf3730a910a4a1643036bb2ecc9705eec69439d
MON5TERMATT <mcbiker2201@gmail.com>
2024-05-24 14:47:35 -0500

    Move files and add install script.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Sovol Theme installer
+#
+# Modified from the very easy to use Zerog installer.
+# https://github.com/ZeroGDesign/zerog-mainsail-theme
+
+# Default Parameters
+KLIPPER_TARGET_DIR="${HOME}/printer_data/config"
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+
+function install_script {
+# Create systemd service file
+    THEME_FILES="${KLIPPER_TARGET_DIR}/.theme"
+
+    # Force remove
+    rm -rf "$THEME_FILES" || true
+
+    echo "Installing theme..."
+    echo "Linking Theme & Git folder to correct directory"
+    ln -sf "${SRCDIR}/.theme" "${KLIPPER_TARGET_DIR}/.theme"
+    ln -sf "${SRCDIR}/.git" "${SRCDIR}/.theme/.git"
+    echo "Theme has been installed, have fun." << EOF
+EOF
+}
+
+install_script
+
+# If something checks status of install
+exit 0
