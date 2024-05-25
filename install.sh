58767f87e6442949c235bb390d6533f0a3a9fa79
MON5TERMATT <mcbiker2201@gmail.com>
2024-05-24 21:27:30 -0500

    Update???

--- install.sh
+++ install.sh
@@ -6,24 +6,19 @@
 
 # Default Parameters
 KLIPPER_TARGET_DIR="${HOME}/printer_data/config"
-SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-function install_script {
 # Create systemd service file
-    THEME_FILES="${KLIPPER_TARGET_DIR}/.theme"
+THEME_FILES="${KLIPPER_TARGET_DIR}/.theme"
 
-    # Force remove
-    rm -rf "$THEME_FILES" || true
+# Force remove
+rm -rf "$THEME_FILES" || true
 
-    echo "Installing theme..."
-    echo "Linking Theme & Git folder to correct directory"
-    ln -sf "${SRCDIR}/.theme" "${KLIPPER_TARGET_DIR}/.theme"
-    ln -sf "${SRCDIR}/.git" "${SRCDIR}/.theme/.git"
-    echo "Theme has been installed, have fun." << EOF
-EOF
-}
-
-install_script
+echo "Installing theme..."
+echo "Linking Theme & Git folder to correct directory"
+ln -sf "${SRCDIR}/.theme" "${KLIPPER_TARGET_DIR}/.theme"
+ln -sf "${SRCDIR}/.git" "${SRCDIR}/.theme/.git"
+echo "Theme has been installed, have fun."
 
 # If something checks status of install
 exit 0
